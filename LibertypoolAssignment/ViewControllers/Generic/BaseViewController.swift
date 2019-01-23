1ccba62d8bdad86f34d7d2b783c342e1c299f7ae
mert-serin <mert@xotv.app>
2019-01-24 01:27:36 +0300

    structure of the project

---
+++ LibertypoolAssignment/ViewControllers/Generic/BaseViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BaseViewController.swift
+//  LibertypoolAssignment
+//
+//  Created by Mert Serin on 24.01.2019.
+//  Copyright © 2019 Mert Serin. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+//    var user:LoginResponseModel?{
+//        get{
+//            return (UIApplication.shared.delegate as! AppDelegate).user
+//        }
+//    }
+//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideKeyboardWhenTap()
+    }
+    
+    func hideKeyboardWhenTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(BaseViewController.didTapAnywhereOnBase))
+        tap.numberOfTouchesRequired = 1
+        tap.numberOfTapsRequired = 1
+        tap.cancelsTouchesInView = true
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func didTapAnywhereOnBase() {
+        self.view.endEditing(true)
+    }
+    
+}
--- LibertypoolAssignment/ViewControllers/Generic/TransactionTabBarController.swift
+++ LibertypoolAssignment/ViewControllers/Generic/TransactionTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TransactionTabBarController.swift
 //  LibertypoolAssignment
 //
 //  Created by Mert Serin on 24.01.2019.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TransactionTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(viewControllers)
     }
-
-
+    
 }
-
